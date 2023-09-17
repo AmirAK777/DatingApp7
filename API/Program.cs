24fffb777545cba9728a5469ef1f9ce7a0746650
AmirAK777 <contactezpro@gmail.com>
2023-09-17 21:37:45 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<DataContext>(opt =>{
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
